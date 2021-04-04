63f91bfe6b324d65b576507be0b172f08e065ffa
ZiCode0 <zed.code.05@gmail.com>
2021-04-05 01:30:39 +1200

    :fire: remove strings :fire:

--- lib/strings.py
+++
@@ -1,4 +0,0 @@
-check_user_exist_cmd = "SELECT check_user_exist($1);"
-gen_and_save_user_salt_cmd = "CALL gen_and_save_user_salt($1, $2);"
-check_user_hash_pass_cmd = "SELECT check_user_hash_pass($1);"
-gen_and_save_token_cmd = "CALL gen_and_save_token($1, $2);"
