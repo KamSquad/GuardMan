e45d7d4b9f7721683c1c0802638dff4a0ff89821
ZiCode0 <zed.code.05@gmail.com>
2021-04-06 14:11:15 +1200

    :fire: clear net request code :fire:

--- lib/network/net_request.py
+++ lib/network/net_request.py
@@ -13,24 +13,3 @@ def make_answer_json(answer_code, body):
     ans_json = {'code': answer_code,
                 'body': body}  # form answer as json
     return ans_json
-
-
-'''
-save_token_request_schema = Schema({
-    'request': And(Use(str)),
-    'body': {
-        'auth': And(Use(str)),
-        'push': And(Use(str)),
-        'univer': And(Use(str)),
-        'fak': And(Use(str)),
-        'state': And(Use(str))
-    }
-})
-
-
-
-def check_token_save_request_struct(dict_obj):
-    check_res = check_schema(inp_conf_schema=save_token_request_schema,
-                             dict_object=dict_obj)
-    return check_res
-'''
