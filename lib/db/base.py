4fe3db53e81af06f65ee0c1c05ec9853d5fc5053
ZiCode0 <zed.code.05@gmail.com>
2021-04-13 14:37:47 +1200

    :recycle: fix base import :recycle:

--- lib/db/base.py
+++ lib/db/base.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from lib.db.setup import Base, UserRole, UserAuth, UserSalt, UserLogin, UserInfo
+from lib.db.setup import Base, UserRole, UserAuth, UserSalt, UserLogin, UserInfo, TokenPerm
 
 
 class DatabaseInstance:
